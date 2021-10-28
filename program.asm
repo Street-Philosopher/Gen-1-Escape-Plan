f532e890e9d411dc52346c9e7f80121f0d33ab8a
AlphaPlayer66 <50373651+AlphaPlayer66@users.noreply.github.com>
2021-10-27 21:38:26 -0700

    Update program.asm

--- program.asm
+++ program.asm
@@ -5,6 +5,7 @@
 BLACK_SQUARE = $54
 WHITE_FRAME  = $55
 
+; in the game boy the payload will be written starting at this address
     org $d901
 
     ld hl,$ff41
@@ -15,13 +16,17 @@ vb:
     ldh ($40),a
 
     ld a,0
-	ldh ($d7),a					; block tile animations
+	ldh ($d7),a					; block tile animations by loading 0 in $ffd7
 
+; this will overwrite the current map data to show on screen 82 unique tiles, that will then be overwritten to show our data
+; hl will hold the VRAM address to write the tile number to
+; a is the tile number we want to write next
+; b and d are used as counters
 fillScreen:
 	ld hl,$9800
 	ld b,$64
 	ld a,BLACK_SQUARE
-fs_loop1:
+fs_loop1:					; start by adding black squares to make the frame
 	ldi (hl),a
 	dec b
 	jr nz,fs_loop1               ; black background
@@ -33,24 +38,24 @@ fs_loop2:
 	jr nz,fs_loop2
 
     ld a,0                       ; go from tile 0 to tile (decimal)82
-    ld d,8                       ; we have 8 rows
+    ld d,8                       ; we have 8 lines in the code
 dataLoop:
     ld b,20
-fs_loop3:                        ; new row
+fs_loop3:                        ; new line, by writing black squares until we're in the right position
     ld (hl),BLACK_SQUARE
     inc hl
     dec b
     jr nz,fs_loop3
-    ld (hl),WHITE_FRAME          ; white tile in the beginning of the row
+    ld (hl),WHITE_FRAME          ; white tile for frame
     inc hl
 
-    ld b,10                      ; 10 dati per riga max
+    ld b,10                      ; there's 10 data tiles per line
 fs_loop4:
-    ldi (hl),a                   ; tile numero "a", aumenta "a" e "hl"
-    inc a
-    dec b
+    ldi (hl),a                   ; we write the tile number to VRAM, then increase VRAM pointer to go to next tile
+    inc a						 ; increase tile number
+    dec b						 ; decrease line tile counter
     jr nz,fs_loop4
-    ld (hl),WHITE_FRAME          ; one white tile for end data
+    ld (hl),WHITE_FRAME          ; one white tile for frame
     inc hl
 
     dec d
