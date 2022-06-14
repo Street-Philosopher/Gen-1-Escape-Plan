5d9d5e270616df7c631ab5b405c08d53dd59f75f
Alpha64 <leoniantonio0@gmail.com>
2022-06-13 19:33:32 -0700

    adjusted build_payload to allow for more than one version of program.asm

--- build_payload.py
+++ build_payload.py
@@ -1,29 +1,36 @@
-import os
+import os, glob
 
 VASM_PATH = "vasmz80_oldstyle_win32.exe"
 VASM_ARGS = "-gbz80 -nocase -chklabels -dotdir -Fbin"
-TEMPFILE_NAME = "temp.bin"
-BUILT_NAME = "build/bytes.txt"
-ASM_PATH = "payload/program_en.asm"
+BUILD_PATH = "build/"
+ASM_PATH = "payload/"
 
 try:
 	print("assembling...")
-	os.system(f'{VASM_PATH} {VASM_ARGS} -o "{TEMPFILE_NAME}" "{ASM_PATH}"')
 
-	print()
-	print("writing the bytes.txt file...")
-	with open(TEMPFILE_NAME, "rb") as ifile, open(BUILT_NAME, "w") as ofile:
-		allbytes = list(ifile.read(-1))
-		res = ""
-		for byte in allbytes:
-			byte = "%0.2X" % byte
-			res += byte + "\n"
-		res = res[:-1]	#get rid of the last endline
-		ofile.write(res)
+	for FILE in glob.glob(ASM_PATH + "program_*.asm"):
+		ext = FILE[len(ASM_PATH + "program_"):-4]		#this will remove everything except the "version"
+		print("building the", ext, "version:")
 
-	print()
-	print("removing temp file...")
-	os.remove(TEMPFILE_NAME)
+		TEMPFILE_NAME = ext + ".temp"					#name of the temp file that will be created
+		BUILT_NAME = BUILD_PATH + f"bytes_{ext}.txt"	#name of the built file
+
+		os.system(f'{VASM_PATH} {VASM_ARGS} -o "{TEMPFILE_NAME}" "{FILE}"')
+
+		print()
+		print("writing the bytes.txt file...")
+		with open(TEMPFILE_NAME, "rb") as ifile, open(BUILT_NAME, "w") as ofile:
+			allbytes = list(ifile.read(-1))
+			res = ""
+			for byte in allbytes:
+				byte = "%0.2X" % byte
+				res += byte + "\n"
+			res = res[:-1]	#get rid of the last endline
+			ofile.write(res)
+
+		print()
+		print("removing temp file...")
+		os.remove(TEMPFILE_NAME)
 
 	print("done!")
 except Exception as e:
