8ab7e0e3141ba9dc51a6ffaf59eb130e05e25003
Alpha64 <leoniantonio0@gmail.com>
2022-06-18 14:08:54 -0700

    interrupts ARE necessary
    +1 byte

--- payload/program_en.asm
+++ payload/program_en.asm
@@ -15,7 +15,7 @@ BOX_DATA  = 0xDA96
 MON_NUMBER= 0xDA80
 
 ; preparation before changing stuff
-	; di
+	di
 VBlankCheck:
 	ldh A,(0x44)	 	; vertical position of scanline
 	cp A, 0x91			; when it's 0x91 we just entered VBlank
@@ -200,4 +200,4 @@ end_overwrite:
 	ld A,0xE1
 	ldh (0x40),A
 	
-	ret
+	reti
