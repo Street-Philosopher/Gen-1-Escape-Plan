0aac969be568c074f0b254bf9c2bdc121a2b0b31
Street-Philosopher <leoniantonio0@gmail.com>
2025-10-14 16:37:09 +0200

    renamed a couple files

---
+++ payload/old.payload.asm
@@ -0,0 +1,303 @@
+; the new version is better on pretty much any regard, except it doesn't yet work on gold and silver
+; i'll leave it here if for any reason someone needs it or wants to look at it, honestly i'm still amazed 2ys later that i managed to optimise this code so much lol
+
+
+
+; i'm sorry to whoever wants to read this code
+
+; version numbers (used for conditional compiling)
+DEF RB_EN = 1
+DEF RB_EU = 2
+DEF GS_EN = 3
+
+; fail if no version is specified
+IF DEF(VERSION) == 0
+	FAIL "No build specified"
+ENDC
+
+INCLUDE "./payload/hardware.asm"
+
+; needed for it to compile. this section will be placed in WRAM at address 0xD901
+; we don't really care since we only use relative jumps, but still
+SECTION "", ROM0
+LOAD "main", WRAMX[$D901]
+
+; TODO: this could be maybe potentially be optimised by removing all the $FF when writing to VRAM which would also double the information density, which would require a rewrite of the program
+; i don't know if you can notice the slow realisation that the idea is not that easy after all, in the comment above
+
+; the program reads data for the currently active box
+; (33*20) + 1 bytes of information
+
+; program constants
+DEF TILES_TO_WRITE = $53
+DEF BLACK_SQUARE   = TILES_TO_WRITE + 1 ; index in memory of the full black tile
+DEF WHITE_SQUARE   = BLACK_SQUARE + 1   ; index in memory of the full white tile
+
+; other constants
+DEF VBLANK_START = $91
+
+; memory addresses
+DEF TILE_DATA = $9000
+DEF MAP_DATA  = $9800
+IF   VERSION == RB_EN
+	def BOX_DATA   = $DA96
+	def MON_NUMBER = $DA80
+ELIF VERSION == RB_EU
+	def BOX_DATA   = $DA9B
+	def MON_NUMBER = $DA85
+ELIF VERSION == GS_EN
+	def BOX_DATA   = $AD82
+	def MON_NUMBER = $AD6C
+ELSE
+	FAIL "Invalid build version"
+ENDC
+
+
+
+PREPARATION:
+; disable interrupts and turn off the LCD
+	di
+wait_vblank:
+	ldh A,[rLY]	 	; vertical position of scanline
+	; CP is just SUB, except it doesn't store a result. we are checking when A-$91 == 0.
+	; since we need to set A to zero afterwards anyways we can use SUB, which does the same of CP (sets the correct flags) AND stores the result (zero, when the comparison succeeds)
+	sub A, VBLANK_START
+	; redo the check if it failed. repeat until we get in vblank
+	jr nz,wait_vblank
+
+	; load zero into the LCD settings which, among other things, turns off the LCD
+	ldh [rLCDC],A
+; another, easier to understand but less optimised, version of this code would be:
+; 	wait_vblank:
+;		ldh A, [rLY]
+;		cp A,  VBLANK_START
+;		jr nz, wait_vblank
+;
+;		ld A, 0x00
+;		ldh [rLCDC],A
+; this second version does the same thing, but takes two extra bytes
+
+
+
+OVERWRITE_MAP_DATA:
+; this will overwrite the current map data to show on screen a frame (black and white) containing all different tiles
+; "HL" will hold the VRAM address containing current tile data, so we can overwrite the map at our likings
+; "C" is the tile number we want to write next
+; "A" contains either the white frame or the black square tile numbers
+; "B" and "D" are used as counters
+;
+; drawing (W means white, D means data):
+;	WWWWWWWWWWWW
+;	WDDDDDDDDDDW
+;	WDDDDDDDDDDW
+;	WDDDDDDDDDDW
+;	WDDDDDDDDDDW
+;	WDDDDDDDDDDW
+;	WDDDDDDDDDDW
+;	WDDDDDDDDDDW
+;	WDDDDDDDDDDW
+;	WDDDDWWWWWWW
+;	WWWWWWWWWWWW
+;
+IF VERSION == RB_EN || VERSION == RB_EU
+	ld BC,$640C
+	; ld B,$64			; to make the code centered in the screen, and to compensate for off-screen tiles
+	; ld C,$0C
+ELIF VERSION == GS_EN
+	ld BC,$840C
+	; ld B,$84
+	; ld C,$0C
+ENDC
+	ld HL,MAP_DATA
+	ld A,BLACK_SQUARE
+fs_loop1:				; start by adding black squares to make the frame
+	ldi [HL],A
+	dec B
+	jr nz,fs_loop1
+	; draws top line of frame
+	inc A	; equivalent to "ld A,WHITE_SQUARE", because WHITE_SQUARE is BLACK_SQUARE + 1
+fs_loop2:
+	ldi [HL],A
+	dec C
+	jr nz,fs_loop2
+
+	ld B,8		; number of lines in the code
+	; "ld C,0" not necessary because C is already zero
+	; C will contain the current tile, so it will get increased whenever we need to place a new one
+dataLoop:	; repeated for each line
+	ld DE,$140A
+	; ld D,20
+	; ld E,10
+	dec A		; does "ld A,BLACK_SQUARE"
+fs_loop3:						; new line, by writing black squares until we're in the right position
+	ldi [HL],A
+	dec D
+	jr nz,fs_loop3
+	inc A				; ld A,WHITE_SQUARE
+	ldi [HL],A			; white tile for frame
+
+	; for 10 data tiles in the line
+fs_loop4:
+	ld [HL],C
+	inc HL
+	inc C							; next tile
+	dec E							; decrease line tile counter
+	jr nz,fs_loop4
+	
+	ldi [HL],A			; one white tile for right frame
+
+	dec B
+	jr nz,dataLoop
+
+	dec A				; ld A,BLACK_SQUARE
+
+IF VERSION == RB_EN || VERSION == RB_EU
+	; the last line is different, having only 4 tiles we need to show. for this reason it has a separate part of the function
+	ld B,20
+fs_loop6:
+	ldi [HL],A
+	dec B
+	jr nz,fs_loop6
+
+	inc A
+	ldi [HL],A
+
+	ld A,C
+
+	ldi [HL],A
+	inc A
+	ldi [HL],A
+	inc A						; 4 tiles, then a white tile
+	ldi [HL],A
+	inc A
+	ldi [HL],A
+
+	ld A,WHITE_SQUARE
+	ld BC,$0714		; load 7 in B and 20 in C, by pairing two registers we save a load
+	; ld B,7
+	; ld C,20
+fs_loop7:						; since we only have 4 data tiles, we fill the other 6 tiles in the line with white frame tiles
+	ldi [HL],A
+	dec B
+	jr nz,fs_loop7
+
+	; last new line
+	dec A		; ld A,BLACK_SQUARE
+fs_loop8:
+	ldi [HL],A
+	dec C
+	jr nz,fs_loop8
+	
+	; bottom row of the frame
+	ld BC,$0C84
+	; ld B,12
+	; ld C,$84
+	inc A; ld a,WHITE_SQUARE
+fs_loop69:
+	ldi [HL],A
+	dec B
+	jr nz,fs_loop69
+	; after the white frame, we cover the rest of the screen with black tiles
+
+ELIF VERSION == GS_EN
+	ld BC,$1484
+	; ld B,14
+	; ld C,0x84
+fs_loop69:
+	ldi [HL],A
+	dec B
+	jr nz,fs_loop69
+
+	ld B,12
+	inc A;ld A,WHITE_SQUARE
+fs_loop_idk:
+	ldi [HL],A
+	dec B
+	jr nz,fs_loop_idk
+ENDC
+	
+	; after the white frame, we cover the rest of the screen with black tiles
+	dec A;ld A,BLACK_SQUARE
+final_loop:
+	ldi [HL],A
+	dec C
+	jr nz,final_loop
+
+
+
+OVERWRITE_TILE_DATA:
+; this overwrites the tile data with the data we need
+; here "D" is used as a counter for the tiles we still have to overwrite
+; "HL" contains the VRAM address containing sprite data, the one we'll need to overwrite
+; "BC" contains the address containing box data
+; "E" is also used as a counter. it counts the number of bytes in each tile (8)
+IF VERSION == GS_EN
+	; in GSC box data is stored in SRAM, so turn it on so we can read it
+	ld A,$0A
+	ld [rRAMG],A
+	ld A,1
+	ld [$6000],A
+	ld A,1
+	ld [rRAMB],A
+ENDC
+
+	ld D,TILES_TO_WRITE			; including 0
+	ld HL,TILE_DATA
+	ld BC,BOX_DATA
+overwriteStuffLoop2:
+	ld E,8			; every tile has 8 bytes
+overwriteStuffLoop:
+	ld A,[BC]			;
+	inc BC				; current_byte = *(data_to_read++)
+	ldi [HL],A			; *(graphics_ptr++) = current_byte
+	ld A,$FF			; *(graphics_ptr++) = $FF          // writing a byte and then $FF will creeate a line with the binary representation of that first byte
+	ldi [HL],A			;
+	dec E
+	jr nz,overwriteStuffLoop	; will loop for all bytes in the tile
+	dec D
+	jr nz,overwriteStuffLoop2	; will loop for every tile in the code
+
+	; an entire column will be just for the number. this saves three bytes
+writeMonNumber:
+	ld BC,$0810
+	; ld B,8
+wmn_black_loop:
+	ld A,[MON_NUMBER]	; TODO: this is an immediate. maybe moving it before everything else and adding instead of loading will do something
+	ldi [HL],A
+	ld A,$FF
+	ldi [HL],A
+	dec B
+	jr nz,wmn_black_loop
+
+	; overwrites the white and black tiles of the frame to be full-white or full-black
+	; ld A,$FF is not needed because we loaded above
+overwrite_5455:
+	; ld C,$10 is done with the paired loop above. since we don't load, the second time we write $100 bytes instead of $10 (the counter starts at $00) but its ok because we overwrite unused tiles
+loop1:				; write 16 (size in bytes of a tile) times ff to create black tile, then adds one to write 00, which creates white tile. this way i don't have to rewrite the function
+	ldi [HL],A
+	dec C
+	jr nz,loop1
+
+	; adds one, and jumps back to the start if there was an overflow (a=0). this way we only jump the first time (a=$FF), AND we get the correct value for "a" to write
+	; O P T I M I S A T I O N S
+	inc A
+	jr z,overwrite_5455
+
+
+
+CLEANUP:
+; reset any important states and return
+IF VERSION == RB_EN || VERSION == RB_EU
+	; turn the screen back on but keep sprites disabled, so the only thing we see is the background we turned into the code
+	ld A,$E1
+	ldh [rLCDC],A
+ELIF VERSION == GS_EN
+	; turn SRAM back off and re-enable LCD with no sprites. this version uses slightly different LCD settings than the one above
+	; A is already zero from the previous operation, so 'xor A,A' is not necessary
+	ld [rRAMG],A
+	ld A,$E3
+	ldh [rLCDC],A
+ENDC
+	; re-enable interrupts and return
+	reti
+
--- payload/payload.asm
+++ payload/payload.asm
@@ -1,298 +1,200 @@
-; i'm sorry to whoever wants to read this code
+; the idea of the second payload is writing a letter code on the screen instead of whatever the fuck i had tried to do previously
+; obviously this has to be done for a single pokemon at a time but eh who cares
 
 ; version numbers (used for conditional compiling)
 DEF RB_EN = 1
 DEF RB_EU = 2
-DEF GS_EN = 3
 
 ; fail if no version is specified
 IF DEF(VERSION) == 0
-	FAIL "No build specified"
+FAIL "No build specified"
+ENDC
+
+IF   VERSION == RB_EN
+	
+ELIF VERSION == RB_EU
+	FAIL "not implemented yet"
+ELSE
+	FAIL "invalid build version"
 ENDC
 
 INCLUDE "./payload/hardware.asm"
 
-; needed for it to compile. this section will be placed in WRAM at address 0xD901
-; we don't really care since we only use relative jumps, but still
 SECTION "", ROM0
-LOAD "main", WRAMX[$D901]
+; LOAD "main", WRAMX[$D901]
 
-; TODO: this could be maybe potentially be optimised by removing all the $FF when writing to VRAM which would also double the information density, which would require a rewrite of the program
-; i don't know if you can notice the slow realisation that the idea is not that easy after all, in the comment above
+; did you know i'm an expert in computer security? i advise you change all your password to be the same. this way it's statistically less likely for one of them to be guessed
+def random_addr_to_overwrite = $D887 ; this is actually the encounter table. have fun
 
-; the program reads data for the currently active box
-; (33*20) + 1 bytes of information
+; magic numbers
+def hTextID = $8c
+def letter_a = $80
+def newline_char = $55
+def terminator_char = $57
 
-; program constants
-DEF TILES_TO_WRITE = $53
-DEF BLACK_SQUARE   = TILES_TO_WRITE + 1 ; index in memory of the full black tile
-DEF WHITE_SQUARE   = BLACK_SQUARE + 1   ; index in memory of the full white tile
-
-; other constants
-DEF VBLANK_START = $91
-
-; memory addresses
-DEF TILE_DATA = $9000
-DEF MAP_DATA  = $9800
-IF   VERSION == RB_EN
-	def BOX_DATA   = $DA96
-	def MON_NUMBER = $DA80
-ELIF VERSION == RB_EU
-	def BOX_DATA   = $DA9B
-	def MON_NUMBER = $DA85
-ELIF VERSION == GS_EN
-	def BOX_DATA   = $AD82
-	def MON_NUMBER = $AD6C
-ELSE
-	FAIL "Invalid build version"
-ENDC
+; addresses
+def wBoxMons = $DA96
+def PrintText = $3C49
+def BankSwitch = $35D6
+def YesNoChoice = $35EC
+def wCurrentMenuItem = $CC26
+def DisplayTextIDInit = $7096
 
 
+; simplified version since we don't have the full symbol table
+MACRO farcall
+	ld b, 1
+	ld hl,\1
+	call BankSwitch
+ENDM
 
-PREPARATION:
-; disable interrupts and turn off the LCD
-	di
-wait_vblank:
-	ldh A,[rLY]	 	; vertical position of scanline
-	; CP is just SUB, except it doesn't store a result. we are checking when A-$91 == 0.
-	; since we need to set A to zero afterwards anyways we can use SUB, which does the same of CP (sets the correct flags) AND stores the result (zero, when the comparison succeeds)
-	sub A, VBLANK_START
-	; redo the check if it failed. repeat until we get in vblank
-	jr nz,wait_vblank
-
-	; load zero into the LCD settings which, among other things, turns off the LCD
-	ldh [rLCDC],A
-; another, easier to understand but less optimised, version of this code would be:
-; 	wait_vblank:
-;		ldh A, [rLY]
-;		cp A,  VBLANK_START
-;		jr nz, wait_vblank
-;
-;		ld A, 0x00
-;		ldh [rLCDC],A
-; this second version does the same thing, but takes two extra bytes
+; we don't have push with 8b regs so here's a simple workaround (we push the 16bit equivalent, then go back by one)
+; not actually used but felt like leaving it here bc i like it for some reason
+MACRO pusha
+	push af
+	inc sp
+ENDM
+MACRO popa
+	dec sp
+	pop af
+ENDM
 
 
 
-OVERWRITE_MAP_DATA:
-; this will overwrite the current map data to show on screen a frame (black and white) containing all different tiles
-; "HL" will hold the VRAM address containing current tile data, so we can overwrite the map at our likings
-; "C" is the tile number we want to write next
-; "A" contains either the white frame or the black square tile numbers
-; "B" and "D" are used as counters
-;
-; drawing (W means white, D means data):
-;	WWWWWWWWWWWW
-;	WDDDDDDDDDDW
-;	WDDDDDDDDDDW
-;	WDDDDDDDDDDW
-;	WDDDDDDDDDDW
-;	WDDDDDDDDDDW
-;	WDDDDDDDDDDW
-;	WDDDDDDDDDDW
-;	WDDDDDDDDDDW
-;	WDDDDWWWWWWW
-;	WWWWWWWWWWWW
-;
-IF VERSION == RB_EN || VERSION == RB_EU
-	ld BC,$640C
-	; ld B,$64			; to make the code centered in the screen, and to compensate for off-screen tiles
-	; ld C,$0C
-ELIF VERSION == GS_EN
-	ld BC,$840C
-	; ld B,$84
-	; ld C,$0C
-ENDC
-	ld HL,MAP_DATA
-	ld A,BLACK_SQUARE
-fs_loop1:				; start by adding black squares to make the frame
-	ldi [HL],A
-	dec B
-	jr nz,fs_loop1
-	; draws top line of frame
-	inc A	; equivalent to "ld A,WHITE_SQUARE", because WHITE_SQUARE is BLACK_SQUARE + 1
-fs_loop2:
-	ldi [HL],A
-	dec C
-	jr nz,fs_loop2
+; alr dumb idea time
+; we don't have any direct way to interact with PC, so we can't really store its value for later use
+; we can't CALL other parts of the program either, as that would require us to have an _absolute jump_ and we'd thus need to know where we are placing the program in memory
+; this is bad bc my guiding principle is that you should be able to use this payload regardless of setup (and thus regardless of memory address)
+; the RST vectors aren't useful either as they just call themselves without ever returning
+; BUT there's a single interrupt in the header which simply RETurns as soon as it is called. thus we can push the PC on the stack this way
+; (could've just as well JPed to any RET in ROM, but i prefer this tbh)
+BEGINNING::
+call $0060
+dec sp
+dec sp
 
-	ld B,8		; number of lines in the code
-	; "ld C,0" not necessary because C is already zero
-	; C will contain the current tile, so it will get increased whenever we need to place a new one
-dataLoop:	; repeated for each line
-	ld DE,$140A
-	; ld D,20
-	; ld E,10
-	dec A		; does "ld A,BLACK_SQUARE"
-fs_loop3:						; new line, by writing black squares until we're in the right position
-	ldi [HL],A
-	dec D
-	jr nz,fs_loop3
-	inc A				; ld A,WHITE_SQUARE
-	ldi [HL],A			; white tile for frame
+; a pkmn is made of 33B. we have 64 possible characters => 6b of info => need to print ceil(33 * 8 / 6) = 44 characters
+; B1: aaaaaabb
+; B2: bbbbcccc
+; B3: ccdddddd
+; => a = (B1 >> 2) & 0b0011_1111
+; => b = (B1 & 0b0000_0011 << 4) + (B2 & 0b1111_0000 >> 4)
+; => c = (B2 & 0b0000_1111 << 2) + (B3 & 0b1100_0000 >> 6)
+; => d = (B3 & 0b0011_1111)
+; repeat 11 times
+GENERATE_STRING_ON_STACK::
+; init
+ld hl,wBoxMons
+ld c,11+1			; the +2 is for easier alignment later. this way we end up PUSHing 48 chars (=> 96 bytes)
 
-	; for 10 data tiles in the line
-fs_loop4:
-	ld [HL],C
-	inc HL
-	inc C							; next tile
-	dec E							; decrease line tile counter
-	jr nz,fs_loop4
-	
-	ldi [HL],A			; one white tile for right frame
+charloop:
+; first char
+ld a,[hl]
+rra					; remember to not put a space, otherwise it becomes `RR A` and takes up twice as much space
+rra
+and a,%0011_1111
+push af				; since the characters are being PUSHed, when decoding we'll have to read them backwards (TODO: how bad can PUSHing be?)
+; second char
+ld a,[hli]			; \
+rla					; |
+rla					; |__ last two bits of B1 get shifted left by 4
+rla					; |
+rla					; |
+and a,%0011_0000	; /
+ld b,a				; we're going to add it later to the other part of the char code
+ld a,[hl]			; \
+rra					; |
+rra					; |__ first half of B2 shifted right (TODO: possibility for optimisation?)
+rra					; |
+rra					; |
+and a,%0000_1111	; /
+add a,b				; add it to the first part computed earlier
+push af
+; third char
+ld a, [hli]			; \
+rla					; |__ first part (second half of B2)
+rla					; |
+and a,%0011_1100	; /
+ld b,a				; store for later computation
+ld a,[hl]			; \
+rra					; |
+rra					; |
+rra					; |__ two most significat digits of B3. repeats 6 times RR A
+rra					; |
+rra					; |
+rra					; |
+and a,%0000_0011	; /
+add a,b
+push af
+; last char
+ld a,[hli]
+and a,%0011_1111
+push af
+dec c
+jr nz, charloop
 
-	dec B
-	jr nz,dataLoop
+; format of string is:
+; 00 (first characters)
+; 4F (next characters)		; we skip this line. looks ugly but works
+; 55 (next characters, repeat this last line as needed)
+; 57 (string terminator that asks for button press)
+PRINT_STRING::
+ld hl, random_addr_to_overwrite
+xor a							; \__ string initiator
+ld [hli], a						; /
 
-	dec A				; ld A,BLACK_SQUARE
+ld b,3							; \
+printloop_pt2:					; |---- 3 lines, 16 chars each
+ld c,16							; /
 
-IF VERSION == RB_EN || VERSION == RB_EU
-	; the last line is different, having only 4 tiles we need to show. for this reason it has a separate part of the function
-	ld B,20
-fs_loop6:
-	ldi [HL],A
-	dec B
-	jr nz,fs_loop6
+printloop:
+pop af							; \
+add a,letter_a					; |--- get the char off the stack, convert it to a printable, and put it on the string
+ld [hli],a						; /
+dec c
+jr nz,printloop
 
-	inc A
-	ldi [HL],A
+ld a, newline_char
+ld [hli],a
+dec b
+jr nz, printloop_pt2
 
-	ld A,C
-
-	ldi [HL],A
-	inc A
-	ldi [HL],A
-	inc A						; 4 tiles, then a white tile
-	ldi [HL],A
-	inc A
-	ldi [HL],A
-
-	ld A,WHITE_SQUARE
-	ld BC,$0714		; load 7 in B and 20 in C, by pairing two registers we save a load
-	; ld B,7
-	; ld C,20
-fs_loop7:						; since we only have 4 data tiles, we fill the other 6 tiles in the line with white frame tiles
-	ldi [HL],A
-	dec B
-	jr nz,fs_loop7
-
-	; last new line
-	dec A		; ld A,BLACK_SQUARE
-fs_loop8:
-	ldi [HL],A
-	dec C
-	jr nz,fs_loop8
-	
-	; bottom row of the frame
-	ld BC,$0C84
-	; ld B,12
-	; ld C,$84
-	inc A; ld a,WHITE_SQUARE
-fs_loop69:
-	ldi [HL],A
-	dec B
-	jr nz,fs_loop69
-	; after the white frame, we cover the rest of the screen with black tiles
-
-ELIF VERSION == GS_EN
-	ld BC,$1484
-	; ld B,14
-	; ld C,0x84
-fs_loop69:
-	ldi [HL],A
-	dec B
-	jr nz,fs_loop69
-
-	ld B,12
-	inc A;ld A,WHITE_SQUARE
-fs_loop_idk:
-	ldi [HL],A
-	dec B
-	jr nz,fs_loop_idk
-ENDC
-	
-	; after the white frame, we cover the rest of the screen with black tiles
-	dec A;ld A,BLACK_SQUARE
-final_loop:
-	ldi [HL],A
-	dec C
-	jr nz,final_loop
+ld a,terminator_char					; TODO: maybe just change the last newline_char to this by addition
+ld [hli],a
 
 
+ENGINE_CALLS::
+ld a,1
+ldh [hTextID],a						; FF8C
+farcall DisplayTextIDInit			; call a function outside of the current ROM bank (need to check argument (0 is ID for start menu))
+ld hl,random_addr_to_overwrite
+call PrintText						; 3C49
 
-OVERWRITE_TILE_DATA:
-; this overwrites the tile data with the data we need
-; here "D" is used as a counter for the tiles we still have to overwrite
-; "HL" contains the VRAM address containing sprite data, the one we'll need to overwrite
-; "BC" contains the address containing box data
-; "E" is also used as a counter. it counts the number of bytes in each tile (8)
-IF VERSION == GS_EN
-	; in GSC box data is stored in SRAM, so turn it on so we can read it
-	ld A,$0A
-	ld [rRAMG],A
-	ld A,1
-	ld [$6000],A
-	ld A,1
-	ld [rRAMB],A
-ENDC
+; ask for next pokemon
+call YesNoChoice
+ld a, [wCurrentMenuItem]
 
-	ld D,TILES_TO_WRITE			; including 0
-	ld HL,TILE_DATA
-	ld BC,BOX_DATA
-overwriteStuffLoop2:
-	ld E,8			; every tile has 8 bytes
-overwriteStuffLoop:
-	ld A,[BC]			;
-	inc BC				; current_byte = *(data_to_read++)
-	ldi [HL],A			; *(graphics_ptr++) = current_byte
-	ld A,$FF			; *(graphics_ptr++) = $FF          // writing a byte and then $FF will creeate a line with the binary representation of that first byte
-	ldi [HL],A			;
-	dec E
-	jr nz,overwriteStuffLoop	; will loop for all bytes in the tile
-	dec D
-	jr nz,overwriteStuffLoop2	; will loop for every tile in the code
+; get back the PC we PUSHed on the stack at the beginning of the program
+; do it here so that we do it regardless of outcome of yes/no
+; if outcome is no we can just not use it and return it, if it is yea we use it
+pop hl
 
-	; an entire column will be just for the number. this saves three bytes
-writeMonNumber:
-	ld BC,$0810
-	; ld B,8
-wmn_black_loop:
-	ld A,[MON_NUMBER]	; TODO: this is an immediate. maybe moving it before everything else and adding instead of loading will do something
-	ldi [HL],A
-	ld A,$FF
-	ldi [HL],A
-	dec B
-	jr nz,wmn_black_loop
+; if YesNoChoice == 0 return
+and a
+ret nz
 
-	; overwrites the white and black tiles of the frame to be full-white or full-black
-	; ld A,$FF is not needed because we loaded above
-overwrite_5455:
-	; ld C,$10 is done with the paired loop above. since we don't load, the second time we write $100 bytes instead of $10 (the counter starts at $00) but its ok because we overwrite unused tiles
-loop1:				; write 16 (size in bytes of a tile) times ff to create black tile, then adds one to write 00, which creates white tile. this way i don't have to rewrite the function
-	ldi [HL],A
-	dec C
-	jr nz,loop1
+; if YesNoChoice == 1
+; ahhh self-modifying code. what could go wrong?
+; first, calculate the address of the byte to modify into HL. then 16bit-add 33 (size of one mon in the box) to it
+ld bc, (GENERATE_STRING_ON_STACK - BEGINNING) - 2		; \___ HL (= start of the program) += (difference between start of the program and address of mon data, that we luckily only use once)
+add hl, bc												; /
 
-	; adds one, and jumps back to the start if there was an overflow (a=0). this way we only jump the first time (a=$FF), AND we get the correct value for "a" to write
-	; O P T I M I S A T I O N S
-	inc A
-	jr z,overwrite_5455
+ld a,33					; 
+ld b, [hl]				; word x = *HL
+add a,b					; low(x) += 33 (number of bytes for one pokemon in the box)
+ld [hli],a				; store changed value of HL
+ret nc					; if there wasn't an overflow we dont need to do any further operations
+inc [hl]				; if there _was_ an overflow, we increment the most significant byte by one to account for that. we already moved the pointer to it with the LD [hli], a
 
-
-
-CLEANUP:
-; reset any important states and return
-IF VERSION == RB_EN || VERSION == RB_EU
-	; turn the screen back on but keep sprites disabled, so the only thing we see is the background we turned into the code
-	ld A,$E1
-	ldh [rLCDC],A
-ELIF VERSION == GS_EN
-	; turn SRAM back off and re-enable LCD with no sprites. this version uses slightly different LCD settings than the one above
-	; A is already zero from the previous operation, so 'xor A,A' is not necessary
-	ld [rRAMG],A
-	ld A,$E3
-	ldh [rLCDC],A
-ENDC
-	; re-enable interrupts and return
-	reti
+ret
 
