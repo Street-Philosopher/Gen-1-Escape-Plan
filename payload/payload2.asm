d3f30adf48eeb953f719eaecabbf3a535655bc57
Street-Philosopher <leoniantonio0@gmail.com>
2025-10-14 13:21:48 +0200

    now working

--- payload/payload2.asm
+++ payload/payload2.asm
@@ -8,7 +8,7 @@ DEF GS_EN = 3
 
 ; fail if no version is specified
 IF DEF(VERSION) == 0
-	FAIL "No build specified"
+FAIL "No build specified"
 ENDC
 
 INCLUDE "./payload/hardware.asm"
@@ -18,27 +18,17 @@ INCLUDE "./payload/hardware.asm"
 SECTION "", ROM0
 LOAD "main", WRAMX[$D901]
 
-; other constants
-DEF VBLANK_START = $91
-
 ; did you know i'm an expert in computer security? i advise you change all your password to be the same. this way it's statistically less likely for one of them to be guessed
-def RANDOM_ADDRESS_TO_OVERWRITE = "lol"
+def RANDOM_ADDRESS_TO_OVERWRITE = $D887 ; this is actually the encounter table. have fun
 
-; memory addresses
-DEF TILE_DATA = $9000
-DEF MAP_DATA  = $9800
-IF   VERSION == RB_EN
-	def BOX_DATA   = $DA96
-	def MON_NUMBER = $DA80
-ELIF VERSION == RB_EU
-	def BOX_DATA   = $DA9B
-	def MON_NUMBER = $DA85
-ELIF VERSION == GS_EN
-	def BOX_DATA   = $AD82
-	def MON_NUMBER = $AD6C
-ELSE
-	FAIL "Invalid build version"
-ENDC
+def hTextID = $8c
+def NEWLINE = $55
+def TEXT_END = $57
+def BOX_DATA = $da96		; check this address i wrote it from memory
+def PrintText = $3c49
+def BankSwitch = $35d6
+def DisplayTextIDInit = $7096
+
 
 ; simplified version since we don't have the full symbol table
 MACRO farcall
@@ -58,41 +48,11 @@ MACRO popa
 	pop af
 ENDM
 
-; format of string has to be:
-; 00 (first characters)
-; 4F (next characters)
-; 55 (next characters, repeat this last line as needed)
-; 57 (string terminator that asks for button press)
 
 
+; TODO: by PUSHing the PC we could see where we are and thus change the starting address of the copied data to go to the next pkmn, regardless of implementation
 
-; si può fare che se premi A ti manda al codice successivo (oppure: sovrascrivi automaticamente un byte che ti mandi al pkmn successivo)
-;
-; charset starts at tile 80 (addr 8800) and end at BF (addr 8BF0)
-; le stringhe iniziano con 00 e finiscono con 57
-; sovrascrivi ultimi 6 tile con i numeri (inizia a F7)
-; un pkmn sono 33B. abbiamo 64 caratteri => 6b di info => servono ceil(33 * 8 / 6) = 44 caratteri
-; schermo GB sono 13x11 (X/Y) tile
-; BBBBBBBBBBBBB
-; BBBBBBBBBBBBB
-; BBBBBBBBBBBBB
-; BBCCCCCCCCCBB
-; BBCCCCCCCCCBB
-; BBCCCCCCCCCBB
-; BBCCCCCCCCCBB
-; BBCCCCCCCC/BB
-; BBBBBBBBBBBBB
-; BBBBBBBBBBBBB
-; BBBBBBBBBBBBB
-;
-; due bitmask
-; b1 = 0b1111_1100
-; b2 = 0b0000_0011
-; si muovono di volta in volta per farti vedere il byte da leggere e il byte successivo da leggere
-; fai AND con il byte di dati da stampare, e poi rimanda al tile
-; sono pur sempre solo 4 i carattero da leggere prima che le mask tornino allo stato iniziale (6-12-18-24, quindi 3B), quindi magari conviene dscriverli direttamente cosi
-
-; METODO 1
+; a pkmn is made of 33B. we have 64 possible characters => 6b of info => need to print ceil(33 * 8 / 6) = 44 characters
 ; B1: aaaaaabb
 ; B2: bbbbcccc
 ; B3: ccdddddd
@@ -100,8 +60,8 @@ ENDM
 ; => b = (B1 & 0b0000_0011 << 4) + (B2 & 0b1111_0000 >> 4)
 ; => c = (B2 & 0b0000_1111 << 2) + (B3 & 0b1100_0000 >> 6)
 ; => d = (B3 & 0b0011_1111)
-; ripeti 11 volte
-
+; repeat 11 times
+GENERATE_STRING_ON_STACK::
 ; init
 ld hl,BOX_DATA
 ld c,11+1			; the +2 is for easier alignment later. this way we end up PUSHing 48 chars (=> 96 bytes)
@@ -109,39 +69,38 @@ ld c,11+1			; the +2 is for easier alignment later. this way we end up PUSHing 4
 charloop:
 ; first char
 ld a,[hl]
-rr a
-rr a
+rra					; remember to not put a space, otherwise it becomes `RR A` and takes up twice as much space
+rra
 and a,%0011_1111
 push af				; since the characters are being PUSHed, when decoding we'll have to read them backwards (TODO: how bad can PUSHing be?)
 ; second char
 ld a,[hli]			; \
-rl a				; |
-rl a				; |__ last two bits of B1 get shifted left by 4
-rl a				; |
-rl a				; |
+rla					; |
+rla					; |__ last two bits of B1 get shifted left by 4
+rla					; |
+rla					; |
 and a,%0011_0000	; /
 ld b,a				; we're going to add it later to the other part of the char code
 ld a,[hl]			; \
-rr a				; |
-rr a				; |__ first half of B2 shifted right (TODO: possibility for optimisation?)
-rr a				; |
-rr a				; |
+rra					; |
+rra					; |__ first half of B2 shifted right (TODO: possibility for optimisation?)
+rra					; |
+rra					; |
 and a,%0000_1111	; /
 add a,b				; add it to the first part computed earlier
 push af
 ; third char
 ld a, [hli]			; \
-rl a				; |__ first part (second half of B2)
-rl a				; |
+rla					; |__ first part (second half of B2)
+rla					; |
 and a,%0011_1100	; /
 ld b,a				; store for later computation
 ld a,[hl]			; \
-rr a				; |
-rr a				; |
-rr a				; |__ two most significat digits of B3. TODO: can this be optimised with a loop?
-rr a				; |
-rr a				; |
-rr a				; |
+ld e,6				; |
+.repeat_rra:		; |
+rra					; |__ two most significat digits of B3. repeats 6 times RR A
+dec e				; |
+jr nz, .repeat_rra	; |
 and a,%0000_0011	; /
 add a,b
 push af
@@ -152,6 +111,11 @@ push af
 dec c
 jr nz, charloop
 
+; format of string is:
+; 00 (first characters)
+; 4F (next characters)		; we skip this line. looks ugly but works
+; 55 (next characters, repeat this last line as needed)
+; 57 (string terminator that asks for button press)
 print_string:
 ld hl, RANDOM_ADDRESS_TO_OVERWRITE
 xor a							; \__ string initiator
@@ -171,16 +135,19 @@ jr nz,printloop
 ld a, NEWLINE
 ld [hli],a
 dec b
-jr nz printloop_pt2
+jr nz, printloop_pt2
 
 ld a,TEXT_END					; TODO: maybe just change the last NEWLINE to this by addition
 ld [hli],a
 
-; assembled instructions (placed at d901) 3E01E08C0601219670CDD6352114D9CD493CC9
-ldh [hTextID],x					; FF8C
-farcall DisplayTextIDInit		; ld b,1; ld hl,$7096; call 35D6 (need to check argument (0 is ID for start menu))
+
+
+; engine calls
+ld a,1
+ldh [hTextID],a						; FF8C
+farcall DisplayTextIDInit			; ld b,1; ld hl,$7096; call 35D6 (need to check argument (0 is ID for start menu))
 ld hl,RANDOM_ADDRESS_TO_OVERWRITE
-call PrintText
+call PrintText						; 3C49
 ret
 
 
@@ -194,51 +161,3 @@ ret
 ; ripeti da assegnazione a char
 
 
-; METODO 2
-; la roba delle bismask non mi convince
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-CLEANUP:
-; reset any important states and return
-IF VERSION == RB_EN || VERSION == RB_EU
-	; turn the screen back on but keep sprites disabled, so the only thing we see is the background we turned into the code
-	ld A,$E1
-	ldh [rLCDC],A
-ELIF VERSION == GS_EN
-	; turn SRAM back off and re-enable LCD with no sprites. this version uses slightly different LCD settings than the one above
-	; A is already zero from the previous operation, so 'xor A,A' is not necessary
-	ld [rRAMG],A
-	ld A,$E3
-	ldh [rLCDC],A
-ENDC
-	; re-enable interrupts and return
-	reti
-
-
-END_PROGRAM:
-txt:
-db $00, $80, $83, $82, $85, $87, $57, $50
